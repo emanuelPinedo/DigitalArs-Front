aff2d0792444c35dcfe2e581ec70a1f0aa0c6584
RamEloisa <ram.eloisalg@gmail.com>
2026-09-04 20:24:27 -0300

    estilos generales

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import './App.css'
 import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import Deposit from './pages/Deposit'
