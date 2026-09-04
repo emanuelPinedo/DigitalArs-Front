5f9d5e49e0b8950319acc66bb1057dc3ee7058ae
Maria Delfina Samperi <delfisamperi@gmail.com>
2026-09-04 20:59:39 -0300

    hu-24 y estilos

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,4 @@
-//import "../../styles/login.css";
+import "../styles/login.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
@@ -82,18 +82,16 @@ function LoginForm() {
         setServerError(""); 
     };
 
-
 return (
     <form className='login-form' onSubmit={handleSubmit}>
-        <h1 className="login-title">Iniciar sesión</h1>
-
+        
         <div className="form-group">
-            <label htmlFor="email">Email</label>
             <input
+                className="login-input"
                 type="email"
                 id="email"
                 name="email"
-                placeholder="Ingrese email"
+                placeholder="Email"
                 value={email}
                 onChange={handleEmailChange}
                 disabled={loading}
@@ -106,13 +104,13 @@ return (
         </div>
 
         <div className="form-group">
-            <label htmlFor="password">Contraseña</label>
             <div className="password-input">
                 <input
+                    className="login-input"
                     type={showPassword ? "text" : "password"}
                     id="password"
                     name="password"
-                    placeholder="Ingrese su contraseña"
+                    placeholder="Password"
                     value={password}
                     onChange={handlePasswordChange}
                     disabled={loading}
@@ -147,12 +145,10 @@ return (
             </div>
         )}
 
-        <button type="submit" disabled={loading}>
-            {loading ? "Ingresando..." : "Ingresar"}
+        <button className="login-button" type="submit" disabled={loading}>
+            {loading ? "..." : "LOGIN"}
         </button>
 
-        <a href="#">Recuperar usuario o clave</a>
-        <a href="#">Registrarse</a>
     </form>
 );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,34 @@
 import LoginForm from '../components/LoginForm';
+// aca hay que importar el componente UI ???
+
+//admin@digitalars.com
+// Admin123!
+// juan.perez@digitalars.com
+// maria.gomez@digitalars.com
+// User123!
+
 
 function Login() {
     return (
         <main className='login-page'>
-            <LoginForm />
+
+            <div className="login-visual">
+                {/* componente Topography */}
+            </div>
+
+            <div className="login-form-container">
+
+                <div className="login-logo">
+                    <img
+                        src="/logo.png"
+                        alt="DigitalArs Wallet"
+                    />
+                </div>
+
+                <LoginForm />
+
+            </div>
+        
         </main>
     );
 }
